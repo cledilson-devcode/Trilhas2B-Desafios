d28f3f3db3c84ab1f1bd5bc27c637ccea4f424d6
Cledilson Filho <cledilsondevoffice@gmail.com>
2025-02-10 18:07:53 -0300

    adicionando resolução da questão 7

--- desafio1.js
+++ desafio1.js
@@ -58,3 +58,16 @@ if (nota >= 7) {
     console.log('Reprovado');
     
 }
+
+//----------------------------------------------------------------------------------
+// Questão 7: Crie duas variáveis, numero1 e numero2, e atribua valores a elas. Verifique se os dois números são iguais e, caso sejam, exiba "Os números são iguais". Caso contrário, exiba "Os números são diferentes".
+
+let numero1 = 10;
+let numero2 = 10;
+if (numero1 == numero2) {
+    console.log('Os números são iguais');
+    
+} else {
+    console.log('Os números são diferentes');
+    
+}
