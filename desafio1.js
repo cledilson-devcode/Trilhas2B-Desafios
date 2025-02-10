9034e1bb3f52e16ae5b6ca14168c19d3837075bf
Cledilson <67111915+cledilson-devcode@users.noreply.github.com>
2025-02-10 16:29:55 -0300

    Adicionando primeira questão do desafio 1

---
+++ desafio1.js
@@ -0,0 +1,8 @@
+// Desafio 1 - BackEnd
+// Aluno: Cledilson Pereira Pinto Filho
+
+// Questão 1: Crie uma variável chamada nome e atribua seu nome a ela. Em seguida, exiba o valor dessa variável.
+
+let nome = 'Cledilson Pereira Pinto Filho';
+console.log(`Meu nome é ${nome}`);
+
