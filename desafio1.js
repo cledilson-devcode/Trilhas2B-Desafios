6ab81ff9f94939c5130b6514e82827871f8f5060
Cledilson Filho <cledilsondevoffice@gmail.com>
2025-02-10 18:05:17 -0300

    Adicionando resolução da questão 6

--- desafio1.js
+++ desafio1.js
@@ -43,3 +43,18 @@ if (idade >= 18) {
     console.log('Você é menor de idade.');
     
 }
+
+//----------------------------------------------------------------------------------
+// Questão 6: Crie uma variável chamada nota e atribua um valor entre 0 e 10. Se a nota for maior ou igual a 7, exiba "Aprovado". Se for entre 5 e 6, exiba "Recuperação". Caso contrário, exiba "Reprovado".
+
+let nota = 10;
+if (nota >= 7) {
+    console.log('Aprovado');
+    
+} else if(nota == 5 || nota == 6){
+    console.log('Recuperação');
+    
+}else{
+    console.log('Reprovado');
+    
+}
