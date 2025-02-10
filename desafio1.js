428380e0b4f7dce12dc4d92e32b5a285ba313998
Cledilson Filho <cledilsondevoffice@gmail.com>
2025-02-10 17:58:03 -0300

    Adicionando resolução da questão 5

--- desafio1.js
+++ desafio1.js
@@ -33,4 +33,13 @@ if (temperatura > 25) {
 }
 
 //----------------------------------------------------------------------------------
-// Questão 5: 
+// Questão 5: Crie uma variável idade e atribua um valor. Se a pessoa for maior de idade (18 ou mais), exiba "Você é maior de idade". Caso contrário, exiba "Você é menor de idade".
+
+idade = 29;
+if (idade >= 18) {
+    console.log('Você é maior de idade.');
+    
+} else {
+    console.log('Você é menor de idade.');
+    
+}
